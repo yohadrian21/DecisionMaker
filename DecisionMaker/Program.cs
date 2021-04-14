f00674467647df3e5d3ecdd05906e01579a2df8c
yohadrian21 <82493723+yohadrian21@users.noreply.github.com>
2021-04-14 14:42:17 +0700

    This is my first Program
    This is my first Program using github. Its about Decision Making when you confuse about it.

---
+++ DecisionMaker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> DecisionList = new List<string>();
+            DecisionList.Add("Yes");
+            DecisionList.Add("No");
+            DecisionList.Add("Maybe");
+            DecisionList.Add("Ask your Friend");
+            DecisionList.Add("Ask your Best Friend");
+            DecisionList.Add("Maybe you should think it");
+            DecisionList.Add("How about NO?");
+            DecisionList.Add("How About Yes");
+
+            var random = new Random();
+            int index = random.Next(DecisionList.Count);
+            Console.WriteLine(DecisionList[index]);
+
+        }
+    }
+}
